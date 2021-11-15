64eed160cab7db7b70a6b7917e8743202d712a88
nabeix <nabeix@users.noreply.github.com>
2021-11-15 21:55:03 +0900

    support formdata

--- packages/cloud-functions-storage-upload/src/index.ts
+++ packages/cloud-functions-storage-upload/src/index.ts
@@ -2,6 +2,7 @@ import * as gcs from "@google-cloud/storage";
 import csprng from "csprng";
 import * as uuid from "uuid";
 import * as mime from "mime-types";
+import Busboy from "busboy";
 
 export interface InitParams {
     clientSecret?: string;
@@ -9,9 +10,107 @@ export interface InitParams {
     keyPrefix?: string;
 }
 
+function createKey(keyPrefix: string | undefined, fileName: string, mimeType: string): string {
+    const ext = (() => {
+        const m = mime.extension(mimeType);
+        if (m) {
+            return m;
+        } else {
+            const chunk = fileName.split(".");
+            return chunk[chunk.length - 1];
+        }
+    })();
+    return (keyPrefix || "") + Date.now() + "/" + csprng(326, 36) + "/" + uuid.v4() + "." + ext;
+}
+function handleFormData(params: InitParams, req: any, res: any): void {
+    const { clientSecret, bucketName, keyPrefix } = params;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    const storage = new gcs.Storage();
+    const bucket = storage.bucket(bucketName);
+
+    const fields: any = {};
+    const busboy = new Busboy({ headers: req.headers });
+    busboy.on("field", (fieldName, value) => {
+        fields[fieldName] = value;
+    });
+    busboy.on("file", (_1, file, fileName, _2, mimeType) => {
+        if (clientSecret && fields["secret"] !== clientSecret) {
+            req.unpipe(busboy);
+            res.status(400).json({
+                message: "Invalid secret(set secret before file)"
+            });
+            return;
+        }
+        const filePath = createKey(keyPrefix || "", fileName, mimeType);
+        const bucketFile = bucket.file(filePath);
+        file.on("error", e => {
+            console.error(e);
+            res.status(500).json({
+                message: "Failed to save file"
+            });
+        }).on("finish", () => {
+            res.json({
+                message: "success",
+                key: filePath,
+                publicUrl: "https://storage.googleapis.com/" + bucketName + "/" + filePath
+            });
+        });
+        file.pipe(bucketFile.createWriteStream());
+    });
+    busboy.on("finish", () => {
+        console.log(`> finish`);
+    });
+    busboy.end(req.rawBody);
+}
+
+async function handleJSON(params: InitParams, req: any, res: any): Promise<void> {
+    const { clientSecret, bucketName, keyPrefix } = params;
+    const { data, fileName, mimeType, secret, addContentDispositionAttachment } = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (clientSecret && clientSecret !== secret) {
+        res.status(401).json({ message: "invalid secret" });
+    }
+
+    const storage = new gcs.Storage();
+    const bucket = storage.bucket(bucketName);
+
+    const saveFile = (key: string, buf: Buffer, mimeType: string) => {
+        return new Promise<void>((resolve, reject) => {
+            const stream = bucket.file(key).createWriteStream({
+                metadata: {
+                    contentType: mimeType
+                },
+                resumable: false
+            });
+            stream.on("error", err => {
+                reject(err);
+            });
+            stream.on("finish", () => {
+                resolve();
+            });
+            stream.end(buf);
+        });
+    };
+    const filePath = createKey(keyPrefix || "", fileName, mimeType);
+    await saveFile(filePath, Buffer.from(data, "base64"), mimeType);
+    if (addContentDispositionAttachment) {
+        const file = bucket.file(filePath);
+        await file.setMetadata({
+            contentDisposition: `attachment;filename=${fileName};filename*=UTF-8''${encodeURIComponent(fileName)}`
+        });
+    }
+    res.json({
+        message: "success",
+        key: filePath,
+        publicUrl: "https://storage.googleapis.com/" + bucketName + "/" + filePath
+    });
+}
+
 // see: https://cloud.google.com/functions/docs/calling/storage
 export function handle(params: InitParams): Function {
-    const { clientSecret, bucketName, keyPrefix } = params;
     return async (req: any, res: any) => {
         if (req.method === "OPTIONS") {
             // CORS support
@@ -22,55 +121,10 @@ export function handle(params: InitParams): Function {
             res.status(204).send("");
             return;
         }
-
-        const { data, fileName, mimeType, secret, addContentDispositionAttachment } = req.body;
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        if (clientSecret && clientSecret !== secret) {
-            res.status(401).json({ message: "invalid secret" });
+        if (req.get("content-type").indexOf("multipart/formdata") === 0) {
+            return handleFormData(params, req, res);
+        } else {
+            return handleJSON(params, req, res);
         }
-
-        const storage = new gcs.Storage();
-        const bucket = storage.bucket(bucketName);
-
-        const saveFile = (key: string, buf: Buffer, mimeType: string) => {
-            return new Promise<void>((resolve, reject) => {
-                const stream = bucket.file(key).createWriteStream({
-                    metadata: {
-                        contentType: mimeType
-                    },
-                    resumable: false
-                });
-                stream.on("error", err => {
-                    reject(err);
-                });
-                stream.on("finish", () => {
-                    resolve();
-                });
-                stream.end(buf);
-            });
-        };
-        const ext = (() => {
-            const m = mime.extension(mimeType);
-            if (m) {
-                return m;
-            } else {
-                const chunk = fileName.split(".");
-                return chunk[chunk.length - 1];
-            }
-        })();
-        const filePath = (keyPrefix || "") + Date.now() + "/" + csprng(326, 36) + "/" + uuid.v4() + "." + ext;
-        await saveFile(filePath, Buffer.from(data, "base64"), mimeType);
-        if (addContentDispositionAttachment) {
-            const file = bucket.file(filePath);
-            await file.setMetadata({
-                contentDisposition: `attachment;filename=${fileName};filename*=UTF-8''${encodeURIComponent(fileName)}`
-            });
-        }
-        res.json({
-            message: "success",
-            key: filePath,
-            publicUrl: "https://storage.googleapis.com/" + bucketName + "/" + filePath
-        });
     };
 }
