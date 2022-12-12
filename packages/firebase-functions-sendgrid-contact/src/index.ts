7fc53831387ec5fe1da21cf5d0691ae133dd2972
nabeix <nabeix@users.noreply.github.com>
2022-12-12 18:16:14 +0900

    fix import

--- packages/firebase-functions-sendgrid-contact/src/index.ts
+++ packages/firebase-functions-sendgrid-contact/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import * as sendGrid from "@sendgrid/mail";
+import sendGrid from "@sendgrid/mail";
 
 admin.initializeApp();
 
